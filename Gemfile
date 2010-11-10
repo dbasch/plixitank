c7fb1fa28e0687ddbdfe757b96ea6f9f3ec510ed
Ben Scofield <git@turrean.com>
2010-11-10 15:24:03 -0600

    initial commit

---
+++ Gemfile
@@ -0,0 +1,12 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.1'
+gem 'pg'
+gem 'json_pure', :require => 'json'
+
+group :development, :test do
+  gem 'sqlite3-ruby', :require => 'sqlite3'
+
+  gem "factory_girl_rails", ">= 1.0.0"
+  gem "shoulda", ">= 2.11.3"
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IndextankDemo::Application.config.secret_token = 'd2127fd9455c56c9a1b681ba2971a44cbd58fd77b293b36efca7d94eecf7fe7de53072a80b144532acce9a4379686fddc8966f4005729510faff6e245bceba21'

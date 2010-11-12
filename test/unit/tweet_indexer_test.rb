e29104a511005fa0082ead10325ab32521447c70
Ben Scofield <git@turrean.com>
2010-11-12 09:37:06 -0600

    minimal testing

---
+++ test/unit/tweet_indexer_test.rb
@@ -0,0 +1,8 @@
+require 'test_helper'
+
+class TweetIndexerTest < ActiveSupport::TestCase
+  context 'The TweetIndexer class' do
+
+
+  end
+end
--- test/unit/tweet_test.rb
+++ test/unit/tweet_test.rb
@@ -1,8 +1,43 @@
 require 'test_helper'
 
 class TweetTest < ActiveSupport::TestCase
-  context 'The Tweet class' do
+  context 'A Tweet' do
+    setup do
+      @tweet = Tweet.new({
+        'Id' => 1,
+        'Message' => 'Hello, Johnny',
+        'UploadDate' => 1234567890,
+        'User' => {'ScreenName' => 'hello'},
+        'ThumbnailUrl' => 'http://foo.bar/baz.jpg'
+      })
+    end
 
+    should 'know its ID' do
+      assert_equal 1, @tweet.id
+    end
 
+    should 'documentize correctly' do
+      hash = {
+        :plixi_id => 1,
+        :text => 'Hello, Johnny',
+        :timestamp => 1234567890,
+        :screen_name => 'hello',
+        :thumbnail_url => 'http://foo.bar/baz.jpg'
+      }
+      assert_equal hash, @tweet.to_document
+    end
+
+    should 'know when it is indexable' do
+      assert @tweet.indexable?
+    end
+
+    should 'pass messages to the data hash' do
+      assert_nothing_raised do
+        @tweet.message
+        @tweet.upload_date
+        @tweet.screen_name
+        @tweet.thumbnail_url
+      end
+    end
   end
 end
